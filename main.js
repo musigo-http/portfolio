1649721a257a57d8ab513acf449546457664d0d3
Mateo A-T <132011455+musigo-http@users.noreply.github.com>
2025-06-22 16:55:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 document.getElementById("btn_musigo").addEventListener("click", function(){
-    window.location.href = "/musigo/about.html";
+    window.location.href = "/portfolio/musigo/about.html";
 });
 
 document.getElementById("btn_myb").addEventListener("click", function(){
-    window.location.href = "/MYB/about.html";
+    window.location.href = "/portfolio/MYB/about.html";
 });
 
 document.getElementById("btn_oralbrvt").addEventListener("click", function(){
-    window.location.href = "/oralbrevet/about.html";
+    window.location.href = "/portfolio/oralbrevet/about.html";
 });
 
 document.getElementById("btn_mllrrap").addEventListener("click", function(){
-    window.location.href = "/meilleursrap2023/about.html";
+    window.location.href = "/portfolio/meilleursrap2023/about.html";
 });
 
 document.getElementById("btn_hashcrack").addEventListener("click", function(){
-    window.location.href = "/hashcrack/about.html";
+    window.location.href = "/portfolio/hashcrack/about.html";
 });
 
 document.getElementById("btn_backdoor").addEventListener("click", function(){
-    window.location.href = "/backdoor/about.html";
+    window.location.href = "/portfolio/backdoor/about.html";
 });

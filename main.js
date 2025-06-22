6532d3dcb0e48c098f8462e0b95c354187ad3849
Mateo A-T <132011455+musigo-http@users.noreply.github.com>
2025-06-22 16:47:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("btn_musigo").addEventListener("click", function(){
+    window.location.href = "/musigo/about.html";
+});
+
+document.getElementById("btn_myb").addEventListener("click", function(){
+    window.location.href = "/MYB/about.html";
+});
+
+document.getElementById("btn_oralbrvt").addEventListener("click", function(){
+    window.location.href = "/oralbrevet/about.html";
+});
+
+document.getElementById("btn_mllrrap").addEventListener("click", function(){
+    window.location.href = "/meilleursrap2023/about.html";
+});
+
+document.getElementById("btn_hashcrack").addEventListener("click", function(){
+    window.location.href = "/hashcrack/about.html";
+});
+
+document.getElementById("btn_backdoor").addEventListener("click", function(){
+    window.location.href = "/backdoor/about.html";
+});
